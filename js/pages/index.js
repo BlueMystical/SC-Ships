d7bbdc44445694ebe8688c8f3b129c379309320d
BlueMystic <jhollman.chacon@gmail.com>
2024-05-21 14:59:27 -0300

    v1.0.5.1

--- js/pages/index.js
+++ js/pages/index.js
@@ -9,14 +9,14 @@ var files;
 window.requestFileSystem = window.requestFileSystem || window.webkitRequestFileSystem;
 var fs = null;
 
-// https://craftpip.github.io/jquery-confirm
-
+Iniciar();
 function Iniciar() {
     try {
         //Load data from a JSON file:
         ShowLoading('Loading..');
+        //console.log('loading..');
         $.getJSON('data/sc_ships_ccu.json', function (data) {
-            shipsData = data; //console.log(shipsData);
+            shipsData = data; // console.log(shipsData);
             SetShipsDataTable_OLD(shipsData);
             HideLoading();
         });
@@ -840,60 +840,3 @@ function supports_html5_storage() {
     }
 }
 
-/*  TO BE DELETED */
-function LogearUsuario() {
-    //Valida al usuario contra el sistema de seguridad, usa AJAX para establecer la conexion, obtiene datos en formato JSON.
-    var bLogedUser = false;
-    try {
-        var pUser = $("#txtUserName").val();
-        var pPass = $("#txtPassword").val();
-
-        if (pUser != '' && pPass != '') {
-            ShowLoading('Validando..'); //<- Muestra el Loader
-
-            //La ruta del servidor se carga desde el archivo de configuracion:
-            //http://localhost/fase3web/?query=u_getinfo&user=244145&pass=5462
-            var pUrl = jsonConfig.web_services.default.url + "/?query=u_getinfo&user=" + pUser + "&pass=" + pPass;
-
-            $.ajax({
-                url: pUrl,
-                dataType: 'json',
-                async: true,
-                success: function (u_login) {
-                    if (u_login !== null) {
-                        //console.log(u_login);
-                        $("#lblLogin").text(u_login.nombre_usuario);
-                        var timeoutID = window.setTimeout(hidePopUp, 500);
-                        usuario_logeado = u_login;
-
-                        //IniciarSesion();
-                        $(":mobile-pagecontainer").pagecontainer("change", '#pageChat', { transition: 'slidefade' });
-
-                        //Transitions: fade, pop, flip, turn, flow, slide, slidefade, slideup, slidedown, none
-
-                    } else {
-                        HideLoading(); //<-Oculta el loader
-                        $("#lblLogin").text("No se pudo validar al usuario!");
-
-                        new Noty({
-                            type: 'error', layout: 'centerLeft', theme: 'relax',
-                            text: "Usuario o Clave Incorrectos, intente de nuevo",
-                            timeout: 5000, progressBar: true, closeWith: ['click', 'button'],
-                            animation: { open: 'noty_effects_open', close: 'noty_effects_close' }
-                        }).show();
-                    }
-
-                },
-                error: function (err) {
-                    console.log('ERROR INESPERADO: ' + err.responseText);
-                    HideLoading(); //<-Oculta el loader
-                    $.alert({ title: 'Error:', content: err.responseText, useBootstrap: false });
-                }
-            });
-            $.mobile.loading("hide"); //<-Oculta el loader
-        }
-    } catch (e) {
-        $.alert({ title: e.name, content: e.message, useBootstrap: false });
-    }
-    return bLogedUser;
-}
